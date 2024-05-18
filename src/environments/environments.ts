2a15dd68ab181753d710b7064d0d41f827547ab1
Teoteo11 <teo.annaro@gmail.com>
2024-05-18 20:46:39 +0200

    feat(): implementation of login with google used auth0

---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  domainAuth0: 'dev-wry0w28eoxtxr8jt.us.auth0.com',
+  clientIdAuth0: '5QMHsUOJNs11Q9EFu9KMgYmFWnKELrkl',
+  redirectUriAuth0: window.location.origin,
+  audienceAuth0: 'https//angular-evolution-api-test'
+
+}
