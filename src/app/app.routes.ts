085868ee362ba639ec3852f43f48d3fe3e3bec2c
Teoteo11 <teo.annaro@gmail.com>
2024-05-18 19:21:01 +0200

    feat(): add initial components and relative routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', loadComponent: () => import('../components/home/home.component') },
+  { path: 'profile', loadComponent: () => import('../components/profile/profile.component') },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
