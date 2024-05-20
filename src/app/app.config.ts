cd53534401675cbc87ae3245ff9fad9a671364b3
Teoteo11 <teo.annaro@gmail.com>
2024-05-20 11:44:33 +0200

    fix(): remove errors by token and interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
+import { AuthService, authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from '../environments/environments';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+export const getToken = () => localStorage.getItem('access_token');
+
+export const controlInterceptor = () => withInterceptors(getToken() ? [authHttpInterceptorFn] : [])
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+    provideHttpClient(controlInterceptor()),
     provideAuth0({
       domain: environment.domainAuth0,
       clientId: environment.clientIdAuth0,
       authorizationParams: {
         redirect_uri: environment.redirectUriAuth0,
         audience: environment.audienceAuth0,
-        scope: 'openid profile email offline_access'
+        scope: 'openid profile email offline_access',
       },
       useRefreshTokens: true,
+      useRefreshTokensFallback: true,
       cacheLocation: 'localstorage', // silent authentication not working in safari, so we use localstorage
       httpInterceptor: {
         allowedList: [
--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -17,6 +18,31 @@ export class NavbarComponent {
   authSignal = toSignal(this.authService.isAuthenticated$)
 
   constructor() {
-    this.authService.isAuthenticated$.subscribe( v => console.log('🟡 SEI LOGGATO? ',v))
+   /*  this.authService.getAccessTokenSilently().pipe(
+      catchError( err => of(
+        console.log('🔴 ERR_ ',err.error),
+        err.error === 'missing_refresh_token' && this.login()
+      )),
+    ).subscribe( v => console.log('prova: ',v)); */
+    this.authService.isAuthenticated$.pipe( catchError((err) => of(console.log('🟡 AUTH ERROR: ',err)) ))
+    .subscribe(
+      v => console.log('🟡 SEI LOGGATO? ', v))
+    // TODO: c'è da smarcare il problema del MISSING REFRESH TOKEN
+    this.authService.error$.subscribe( 
+      error =>{
+        console.log('🔴 ERRORE LOGIN: ', error);
+        if (error && error.message.includes('paradigma')) {
+          console.log('TROVATO ERRORE ORA TI SLOGGO');
+          alert('Devi loggarti con un account Paradigma!')
+          this.authService.logout();
+        }
+      }
+    );
+  }
+
+  login = () => {
+    this.authService.loginWithRedirect().pipe(
+      catchError((err) => of(console.log('err_ ', err))))
+      .subscribe( i => console.log('I: ',i));
   }
 }
